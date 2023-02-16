ba4da2e4a14d9ac5cfe346d7caf947883acf5b26
Plashon <fourza12poobate@gmail.com>
2023-02-16 16:24:41 +0700

    16-2-2566

--- index.js
+++ index.js
@@ -2,19 +2,33 @@
 let menu = document.querySelector("#menu-bars");
 let navbar = document.querySelector("header .flex .navbar");
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-xmark');
-    navbar.classList.toggle('active');
+menu.onclick = () => {
+  menu.classList.toggle("fa-xmark");
+  navbar.classList.toggle("active");
 };
 //show search
-let search = document.querySelector('#search-icon');
-search.onclick = () =>{
-let search_form = document.querySelector('#search-form');
-     search_form.classList.toggle('active');
-} 
+let search = document.querySelector("#search-icon");
+search.onclick = () => {
+  let search_form = document.querySelector("#search-form");
+  search_form.classList.toggle("active");
+};
 //close search
-let close_icon = document.querySelector('#close');
-close_icon.onclick = () =>{
-let search_form = document.querySelector('#search-form');
-     search_form.classList.remove('active');
-} 
+let close_icon = document.querySelector("#close");
+close_icon.onclick = () => {
+  let search_form = document.querySelector("#search-form");
+  search_form.classList.remove("active");
+};
+//swiper function
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
